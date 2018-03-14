ee97e4ea44ef762ae735cfc48f953fe3627c03af
merrillrudd <merrillrudd@gmail.com>
2018-03-13 23:05:10 -0800

    updated day 3

--- Day3/3a_Simulation/agestructure_sim2/runsim.R
+++ Day3/3a_Simulation/agestructure_sim2/runsim.R
@@ -1,18 +1,25 @@
+setwd("C:\\merrill\\ADMB_course\\Day3\\3a_Simulation\\agestructure_sim2")
+
+library(R2admb)
+
 source("read.admb.R")
+
 run.Simulation=function(N=10)
 {
     theta <<- NULL
     for(i in sample(1:1000,N))
     {
-        arg = paste("./sca -sim", i)
-        system(arg)
+        arg = paste("-sim", i)
+        run_admb("SCA", extra.args=arg)
         print(arg)
-        P=read.fit("sca")
+        P=read.fit("SCA")
         theta<<-rbind(theta, P$est[1:6])
     }
 
 }
+compile_admb("SCA")
 run.Simulation(10)
+
 tvalues=c(12000,10,12000,0.5,2,0.75)
 names=c("ro","cr","rbar","fbar","ahat","ghat")
 trtheta=cbind(exp(theta[,1:4]),theta[,5:6])
